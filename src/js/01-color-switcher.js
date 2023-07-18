68deb44cfb9d11122238f371fb5bdfaa45212e9e
Alex <alexlipskyi@icloud.com>
2023-07-18 19:35:10 +0300

    hello

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+// variables
+
+const startBtn = document.querySelector(`button[data-start]`);
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+
+// listeners
+
+startBtn.addEventListener('click', backgroundChange);
+
+stopBtn.addEventListener('click', backgroundChangeBreak);
+
+// functions
+
+function backgroundChange() {
+  startBtn.disabled = true;
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function backgroundChangeBreak(e) {
+  startBtn.disabled = false;
+
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,109 @@
+
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+console.log(form);
+
+form.addEventListener("submit",createPromise);
+
+   
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve,reject) => {
+
+    if (shouldResolve) {
+      resolve("Hello,I am resolve"); // Fulfill
+ } 
+  
+      reject("I am reject!");
+  });
+ 
 }
+
+
+
+createPromise().then(onSuccess).catch(onError);
+
+function onSuccess(resolve) {
+  Notiflix.Notify.success(resolve);
+}
+
+function onError(reject) {
+
+  Notiflix.Notify.failure(reject);
+}
+
+
+//!=================================================
+
+// const makePromise = () => {
+
+//   return new Promise((resolve,reject) => {
+
+//     const passed = Math.random()>0.5;
+
+//     if(passed) {
+
+//       resolve("Ку-ку это resolve");
+
+//     }
+
+//     reject("Все пропало, это reject");
+
+//   })
+// }
+
+// makePromise().then(res => console.log(res)).catch(err => console.log(err));
+// const makeOrder = dish => {
+
+//   const DELAY = 1000;
+//   return  new Promise((resolve,reject) => {
+
+//     const rand = Math.random() > 0.5;
+
+//     setTimeout(() => {
+
+//       if(rand) {
+
+//         resolve(`${dish} is being prepared`);
+//       }
+
+//       reject(`${dish} can't be prepared`);
+//     })
+
+//   },DELAY);
+// }
+
+// makeOrder('пирожок').then(onSuccess).catch(onError);
+
+// function onSuccess(result) {
+
+//   console.log(result);
+
+// }
+
+// function onError (error) {
+
+//   console.log(error);
+// }
+
+// function fetchPokemonId (id) {
+
+//   return  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+//   .then(r => r.json())
+
+// }
+
+// fetchPokemonId(1).then(fetchOnSuccess).catch(fetchOnError);
+
+// function fetchOnSuccess(pokemon) {
+
+//   console.log(pokemon);
+// }
+
+// function fetchOnError(error)  {
+
+//   console.log(error);
+// }
